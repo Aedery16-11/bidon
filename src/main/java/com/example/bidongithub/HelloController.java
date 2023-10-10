07538f4f4ece9d826c08d25a0359aa25d01ae5ac
Aedery16-11 <aedery.16.11@gmail.com>
2023-10-10 17:24:47 +0200

    déclaration de la variable toto

--- src/main/java/com/example/bidongithub/HelloController.java
+++ src/main/java/com/example/bidongithub/HelloController.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+int toto=8;
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
